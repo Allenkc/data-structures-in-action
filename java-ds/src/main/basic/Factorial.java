46ad248c89c4c07b4671bb6a5afc6cd2a8bcb9ba
AllenKC <aa87220@gmail.com>
2019-09-11 11:50:02 +0800

    [add] fac iteratively

--- java-ds/src/main/basic/Factorial.java
+++ java-ds/src/main/basic/Factorial.java
@@ -4,22 +4,40 @@ public class Factorial {
 
     /**
      * 關鍵是遞迴式:
-     *
+     * <p>
      * n! = {
-     *      1 , if n=0
-     *      (n-1)! * n , if n > 0
+     * 1 , if n=0
+     * (n-1)! * n , if n > 0
      * }
-     *
      *
      * @param n
      * @return
      */
-    public static int doFactorial(int n) {
+    public static int doFactorialRecursively(int n) {
 
-        if(n == 0){
+        if (n == 0) {
             return 1;
         }
 
-        return doFactorial(n-1)*n;
+        return doFactorialRecursively(n - 1) * n;
+    }
+
+    public static int doFactorialIteratively(int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        int i;
+        int product = 1;
+
+        for (i = 1; i <= n; i++) {
+
+            product = product * i;
+
+        }
+
+        return product;
+
     }
 }
--- java-ds/src/test/basic/FactorialTest.java
+++ java-ds/src/test/basic/FactorialTest.java
@@ -9,14 +9,18 @@ public class FactorialTest {
     @Test
     public void testFactorial() {
 
-        int result = Factorial.doFactorial(5);
+        int result = Factorial.doFactorialRecursively(5);
         Assertions.assertEquals(120, result);
 
-        int result2 = Factorial.doFactorial(0);
+        int result2 = Factorial.doFactorialRecursively(0);
         Assertions.assertEquals(1, result2);
 
-        int result3 = Factorial.doFactorial(10);
-        Assertions.assertEquals(3628800 ,result3);
+        int result3 = Factorial.doFactorialRecursively(10);
+        Assertions.assertEquals(3628800, result3);
+
+        Assertions.assertEquals(120, Factorial.doFactorialIteratively(5));
+        Assertions.assertEquals(1, Factorial.doFactorialIteratively(0));
+        Assertions.assertEquals(3628800, Factorial.doFactorialIteratively(10));
 
     }
 }
