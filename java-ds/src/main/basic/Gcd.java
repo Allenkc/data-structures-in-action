2cc7d66dbb0ad9701f3f48d8a1f216a847a756ef
AllenKC <aa87220@gmail.com>
2019-09-12 10:47:30 +0800

    [add] gcd iterative

--- java-ds/src/main/basic/Gcd.java
+++ java-ds/src/main/basic/Gcd.java
@@ -8,22 +8,35 @@ public class Gcd {
 
     /**
      * 關鍵是Recursive Formula
-     *
+     * <p>
      * GCD(A , B) = {
-     *     B , if (A mod B) = 0
-     *     GCD(B , A mod B) , otherwise
+     * B , if (A mod B) = 0
+     * GCD(B , A mod B) , otherwise
      * }
+     *
      * @param a
      * @param b
      * @return gcd result
      */
-    public static int get(int a , int b) {
+    public static int get(int a, int b) {
 
-        if ( (a%b) == 0){
+        if (b == 0) return a;
+
+        if ((a % b) == 0) {
             return b;
         }
 
-        return get(b , a%b);
+        return get(b, a % b);
 
+    }
+
+    // iteratively
+    public static int get2(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }
--- java-ds/src/test/basic/GcdTest.java
+++ java-ds/src/test/basic/GcdTest.java
@@ -10,5 +10,10 @@ public class GcdTest {
     public void gcdTest(){
         Assertions.assertEquals(Gcd.get(12,2),2);
         Assertions.assertEquals(Gcd.get(18,32),2);
+        Assertions.assertEquals(Gcd.get(1024,0),1024);
+
+        Assertions.assertEquals(Gcd.get2(12,2),2);
+        Assertions.assertEquals(Gcd.get2(18,32),2);
+        Assertions.assertEquals(Gcd.get2(1024,0),1024);
     }
 }
